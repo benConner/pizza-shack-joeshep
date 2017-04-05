4e74f52f22d40eb97a20c7361d18ab9130f2da94
Ben Conner <ben.conner40@gmail.com>
2017-04-05 11:57:18 -0500

    saving a info from contact page into contact table in db

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const { Router } = require('express')
-const { show } = require('../controllers/contact')
+const { show, addContact } = require('../controllers/contact')
 const router = Router()
 
 router.get('/contact', show)
+router.post('/contact', addContact)
 
 module.exports = router;
